9c130af10802cec9cdbfda52b2ccfdfc7657d96b
kevinkim910408 <kevinkim0408@naver.com>
2020-10-04 21:25:29 +0900

    TileMap, Player, BGM work
    TileMap
    Player - Need to make Jump, Animation, shooting
    BGM

---
+++ Assets/_Scripts/PlayerCharacter.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCharacter : MonoBehaviour
+{
+    #region Variables
+
+    [SerializeField]
+    float moveSpeed = 3.0f;
+
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMove();
+    }
+
+
+    void PlayerMove()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+
+        Vector3 moveVector = new Vector3(moveX, 0, 0) * moveSpeed * Time.deltaTime;
+        transform.Translate(moveVector);
+    }
+}
