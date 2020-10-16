ecc21e43e2f994869825e647b546a57f8812a35f
kevinkim910408 <kevinkim0408@naver.com>
2020-10-16 16:42:14 +0900

    Sound Work
    SoundManager (object + script) is managing sounds from now.

---
+++ Assets/_Scripts/_Managers/SoundManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Sound
+{
+    public string soundName;
+    public AudioClip audioClip;
+}
+
+
+public class SoundManager : MonoBehaviour
+{
+    #region Variables
+    [Header("Background Music")]
+
+    [SerializeField]
+    Sound[] bgmStorage; // index[0] - GameScene BGM, 
+
+    [SerializeField]
+    AudioSource bgmPlayer;
+
+
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayBGM();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // GameScene BGM
+    public void PlayBGM() 
+    {
+        bgmPlayer.clip = bgmStorage[0].audioClip;
+        bgmPlayer.Play();
+    }
+}
