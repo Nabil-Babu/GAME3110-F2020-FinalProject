b0d6da4340f0938483b18bc85049fb74381c0089
Nabil Babu <nabil.babu20@gmail.com>
2020-10-17 09:51:20 -0400

    Death Box for players and Faster Projectile

---
+++ Assets/_Scripts/PlayerSpawner.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpawner : MonoBehaviour
+{
+    public GameObject spawnPoint;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = spawnPoint.transform.position;
+    }
+
+    public void Respawn()
+    {
+        transform.position = spawnPoint.transform.position;
+    }
+
+
+}
