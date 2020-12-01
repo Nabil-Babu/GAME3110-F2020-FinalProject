22f826f9c512cfcefe0961589ea51324a12cce60
Teurowel <teurowel@gmail.com>
2020-12-01 18:03:58 +0900

    Added Unity.Transport Package

---
+++ Assets/_Scripts/Network/NetworkClient.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using Unity.Collections;
+//using Unity.Networking.Transport;
+
+public class NetworkClient : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //Start to connect to match making server
+    public void StartConnetToMatchMakingServer()
+    {
+        //m_Driver = NetworkDriver.Create(); //creat socket
+
+        //m_Connection = default(NetworkConnection);
+
+        ////serverIP = "3.15.221.96";
+        ////serverIP = "127.0.0.1";
+        //var endpoint = NetworkEndPoint.Parse(serverIP, serverPort);
+        //m_Connection = m_Driver.Connect(endpoint); //connect to server
+    }
+}
